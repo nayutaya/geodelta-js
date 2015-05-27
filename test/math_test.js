1fd264c1fefa0a14c21b539ee99532ea205c05f5
Yuya Kato <yuyakato@gmail.com>
2015-05-27 19:40:14 +0900

    テストの説明を追加

--- test/math_test.js
+++ test/math_test.js
@@ -4,7 +4,7 @@ var math = require("../math.js");
 
 describe("math", function() {
   describe(".sinh", function() {
-    it("", function() {
+    it("RubyのMath.sinhと同じ動きをすること", function() {
       assert(Math.abs(math.sinh(-2.0) - -3.6268604078470186) < 1e-15);
       assert(Math.abs(math.sinh(-1.0) - -1.1752011936438014) < 1e-15);
       assert.equal(0.0, math.sinh(0.0));
@@ -14,7 +14,7 @@ describe("math", function() {
   });
 
   describe(".cosh", function() {
-    it("", function() {
+    it("RubyのMath.coshと同じ動きをすること", function() {
       assert(Math.abs(math.cosh(-2.0) - +3.7621956910836314) < 1e-15);
       assert(Math.abs(math.cosh(-1.0) - +1.5430806348152437) < 1e-15);
       assert.equal(1.0, math.cosh(0.0));
@@ -24,7 +24,7 @@ describe("math", function() {
   });
 
   describe(".tanh", function() {
-    it("", function() {
+    it("RubyのMath.tanhと同じ動きをすること", function() {
       assert(Math.abs(math.tanh(-2.0) - -0.9640275800758169) < 1e-15);
       assert(Math.abs(math.tanh(-1.0) - -0.7615941559557649) < 1e-15);
       assert.equal(0.0, math.tanh(0.0));
@@ -34,7 +34,7 @@ describe("math", function() {
   });
 
   describe(".atanh", function() {
-    it("", function() {
+    it("RubyのMath.atanhと同じ動きをすること", function() {
       assert(Math.abs(math.atanh(-0.2) - -0.2027325540540822) < 1e-15);
       assert(Math.abs(math.atanh(-0.1) - -0.1003353477310756) < 1e-15);
       assert.equal(0.0, math.atanh(0.0));
@@ -44,7 +44,7 @@ describe("math", function() {
   });
 
   describe(".mod", function() {
-    it("", function() {
+    it("Rubyの剰余と同じ動きをすること", function() {
       assert.equal(0, math.mod(0, 1));
       assert.equal(0, math.mod(1, 1));
 
