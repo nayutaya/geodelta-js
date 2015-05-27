1e2de9d58c129e2dce7e94eda39114aa2994d224
Yuya Kato <yuyakato@gmail.com>
2015-05-28 02:12:06 +0900

    GeoDeltaコードをデコードする関数を追加

--- import/geodelta16/js/lib/encoder.js
+++ import/geodelta16/js/lib/encoder.js
@@ -5,22 +5,4 @@ var geodelta = geodelta || {};
 geodelta.encoder = geodelta.encoder || {};
 
 (function() {
-  // GeoDeltaコードをデコードする
-  geodelta.encoder.decode = function(code) {
-    if ( code == null || code.length == 0 )
-    {
-      // TODO: throw new IllegalArgumentException();
-      return null;
-    }
-    else if ( code.length == 1 )
-    {
-      return [geodelta.encoder.decodeWorldDelta(code.charAt(0))];
-    }
-    else
-    {
-      var worldId = geodelta.encoder.decodeWorldDelta(code.charAt(0));
-      var subIds  = geodelta.encoder.decodeSubDelta(code.substring(1));
-      return [worldId].concat(subIds);
-    }
-  };
 })();
--- test/encoder_test.js
+++ test/encoder_test.js
@@ -145,9 +145,7 @@ function test_encodeWorldDelta__invalidArg2()
 */
 
   describe(".decodeSubDelta", function() {
-    var assertArrayEquals = function(expected, actual) {
-      expect(actual).to.eql(expected);
-    };
+    var assertArrayEquals = function(expected, actual) { expect(actual).to.eql(expected); };
     it("サブデルタコードをデコードできること", function() {
       assertArrayEquals([0, 0], encoder.decodeSubDelta("2"));
       assertArrayEquals([0, 1], encoder.decodeSubDelta("3"));
@@ -259,6 +257,62 @@ function test_encodeWorldDelta__invalidArg2()
     public void encode__invalidArg2()
     {
         geodelta.encoder.encode(new byte[0]);
+    }
+*/
+
+  describe(".decode", function() {
+    var assertArrayEquals = function(expected, actual) { expect(actual).to.eql(expected); };
+    it("GeoDeltaコードをデコードできること", function() {
+      assertArrayEquals([0],          encoder.decode("Z"));
+      assertArrayEquals([0, 1],       encoder.decode("ZM"));
+      assertArrayEquals([0, 1, 2],    encoder.decode("Z8"));
+      assertArrayEquals([0, 1, 2, 3], encoder.decode("Z8P"));
+      assertArrayEquals([7],          encoder.decode("R"));
+      assertArrayEquals([7, 3],       encoder.decode("RP"));
+      assertArrayEquals([7, 3, 2],    encoder.decode("RH"));
+      assertArrayEquals([7, 3, 2, 1], encoder.decode("RHM"));
+    });
+  });
+
+/*
+    @Test(expected = IllegalArgumentException.class)
+    public void decode__invalidArg1()
+    {
+        geodelta.encoder.decode(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void decode__invalidArg2()
+    {
+        geodelta.encoder.decode("");
+    }
+
+    @Test
+    public void randomEncodeAndDecode()
+    {
+        final Random r = new Random();
+
+        for ( int i = 0; i < 1000; i++ )
+        {
+            final byte[] ids = createRandomDeltaIds(r, 20);
+            final String encoded1 = geodelta.encoder.encode(ids);
+            final byte[] decoded1 = geodelta.encoder.decode(encoded1);
+            final String encoded2 = geodelta.encoder.encode(decoded1);
+            assertArrayEquals(ids, decoded1);
+            assertEquals(encoded1, encoded2);
+        }
+    }
+
+    // FIXME: refactoring
+    private byte[] createRandomDeltaIds(final Random random, final int level)
+    {
+        final byte[] ids = new byte[random.nextInt(level) + 1];
+        ids[0] = (byte)random.nextInt(8);
+        for ( int i = 1; i < ids.length; i++ )
+        {
+            ids[i] = (byte)random.nextInt(4);
+        }
+        return ids;
     }
 */
 
