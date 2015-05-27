70505ec495bbbeaad595b8f3fd642bf017213191
Yuya Kato <yuyakato@gmail.com>
2015-05-27 19:01:53 +0900

    負の値のテストケースを追加

--- test/math_test.js
+++ test/math_test.js
@@ -5,9 +5,11 @@ var math = require("../math.js");
 describe("math", function() {
   describe(".sinh", function() {
     it("", function() {
+      assert(Math.abs(math.sinh(-2.0) - -3.6268604078470186) < 1e-10);
+      assert(Math.abs(math.sinh(-1.0) - -1.1752011936438014) < 1e-10);
       assert.equal(0.0, math.sinh(0.0));
-      assert(Math.abs(math.sinh(1.0) - 1.1752011936438014) < 1e-10);
-      assert(Math.abs(math.sinh(2.0) - 3.6268604078470186) < 1e-10);
+      assert(Math.abs(math.sinh(+1.0) - +1.1752011936438014) < 1e-10);
+      assert(Math.abs(math.sinh(+2.0) - +3.6268604078470186) < 1e-10);
     });
   });
 });
