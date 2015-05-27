36e7729b57b5b4656052a76b39336ab8e99e029e
Yuya Kato <yuyakato@gmail.com>
2015-05-27 19:05:44 +0900

    テストにて比較する値の精度を変更

--- test/math_test.js
+++ test/math_test.js
@@ -5,11 +5,11 @@ var math = require("../math.js");
 describe("math", function() {
   describe(".sinh", function() {
     it("", function() {
-      assert(Math.abs(math.sinh(-2.0) - -3.6268604078470186) < 1e-10);
-      assert(Math.abs(math.sinh(-1.0) - -1.1752011936438014) < 1e-10);
+      assert(Math.abs(math.sinh(-2.0) - -3.6268604078470186) < 1e-15);
+      assert(Math.abs(math.sinh(-1.0) - -1.1752011936438014) < 1e-15);
       assert.equal(0.0, math.sinh(0.0));
-      assert(Math.abs(math.sinh(+1.0) - +1.1752011936438014) < 1e-10);
-      assert(Math.abs(math.sinh(+2.0) - +3.6268604078470186) < 1e-10);
+      assert(Math.abs(math.sinh(+1.0) - +1.1752011936438014) < 1e-15);
+      assert(Math.abs(math.sinh(+2.0) - +3.6268604078470186) < 1e-15);
     });
   });
 
