57acd2c4158cb4a1481a39bd0bf647705f2a201b
Yuya Kato <yuyakato@gmail.com>
2015-05-27 17:59:04 +0900

    パッケージ情報のひな形を追加

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "geodelta",
+  "version": "0.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Yuya Kato (Nayutaya Inc.)",
+  "license": "Apache License Version 2.0"
+}
